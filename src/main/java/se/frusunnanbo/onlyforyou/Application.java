64a2aa27d7c99d73331078889d99c7a7013971b7
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2017-10-10 09:26:16 +0200

    Elm frontend embryo

--- src/main/java/se/frusunnanbo/onlyforyou/Application.java
+++ src/main/java/se/frusunnanbo/onlyforyou/Application.java
@@ -2,12 +2,15 @@ package se.frusunnanbo.onlyforyou;
 
 import com.google.gson.Gson;
 import se.frusunnanbo.onlyforyou.input.UserData;
+import spark.Spark;
 
 import static spark.Spark.get;
 
 public class Application {
     public static void main(String[] args) {
         Gson gson = new Gson();
+
+        Spark.externalStaticFileLocation(System.getProperty("frontend.root"));
 
         get("/userdata", (req, res) -> UserData.users(), gson::toJson);
     }
