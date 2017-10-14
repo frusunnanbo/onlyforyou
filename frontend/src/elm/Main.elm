e2c39999c92dced3d90b51a89a3a5228b8c2d054
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2017-10-14 21:36:37 +0200

    Showing ratings or empty cells

--- frontend/src/elm/Main.elm
+++ frontend/src/elm/Main.elm
@@ -1,7 +1,86 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Html exposing (table, tr, th, td, text, beginnerProgram, Html)
+import Dict exposing (Dict)
 
 
 main =
-    text "User / Item matrix goes here"
+    beginnerProgram { model = init, update = update, view = view }
+
+
+type alias Item = String
+
+type alias Rating =
+    { item : Item
+    , score : Float
+    }
+
+
+type alias User =
+    { name : String
+    , ratings : List Rating
+    }
+
+
+type alias Model =
+    List User
+
+
+init : List User
+init =
+    [ User "Anna" [ Rating "Greta Gris" 5, Rating "Bon" 3 ]
+    , User "Britta" [ Rating "Bon" 2, Rating "Nobelfesten" 5 ]
+    , User "Cilla" [ Rating "Vår tid är nu" 1, Rating "Medan vi dör" 5 ]
+    , User "Daniela" [ Rating "Skam" 3, Rating "Vår tid är nu" 3 ]
+    ]
+
+
+type Msg
+    = Next
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Next ->
+            model
+
+
+view : Model -> Html Msg
+view model =
+    let
+        items = [ "Greta Gris", "Bon", "Nobelfesten", "Vår tid är nu", "Medan vi dör", "Skam" ]
+    in
+        table []
+            (heading items :: List.map (row items) model)
+
+heading : List Item -> Html Msg
+heading items =
+    tr []
+        (th [] [text ""] :: List.map (\item ->  th [] [ text item ]) items)
+
+row : List Item -> User -> Html Msg
+row items user =
+    tr []
+        (td [] [ text user.name ] :: (ratings items) user.ratings)
+
+ratings : List Item -> List Rating -> List (Html Msg)
+ratings items ratings =
+    List.map (rating ratings) items
+
+rating : List Rating -> Item -> Html Msg
+rating ratings item =
+    td [] [ score ratings item ]
+
+score : List Rating -> Item -> Html Msg
+score ratings item =
+    Dict.get item (toDict ratings)
+        |> Maybe.map toString
+        |> Maybe.withDefault ""
+        |> text
+
+toDict : List Rating -> Dict Item Float
+toDict ratings =
+    ratings
+        |> List.map (\rating -> (rating.item, rating.score))
+        |> Dict.fromList
--- src/main/java/se/frusunnanbo/onlyforyou/Application.java
+++ src/main/java/se/frusunnanbo/onlyforyou/Application.java
@@ -5,12 +5,14 @@ import se.frusunnanbo.onlyforyou.input.UserData;
 import spark.Spark;
 
 import static spark.Spark.get;
+import static spark.Spark.staticFiles;
 
 public class Application {
     public static void main(String[] args) {
         Gson gson = new Gson();
 
         Spark.externalStaticFileLocation(System.getProperty("frontend.root"));
+        staticFiles.registerMimeType("html", "text/html; charset=utf-8");
 
         get("/userdata", (req, res) -> UserData.users(), gson::toJson);
     }
